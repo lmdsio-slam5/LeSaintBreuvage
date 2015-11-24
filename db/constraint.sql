21a93a91db717697b0d867b70ec94e56b00be0c6
Aurélien Aubert <aurelien.aubert@outlook.com>
2015-11-24 11:21:05 +0100

    Mise à jour de la bdd
    Mise à jour de la bdd

---
+++ db/constraint.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `biere` ADD CONSTRAINT `FK_biere_categorie` FOREIGN KEY (`BIE_CodeBiere`) REFERENCES `lesaintbreuvage`.`categorie`(`CAT_CodeCategorieBiere`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `prix` ADD CONSTRAINT `FK_biere_prix` FOREIGN KEY (`BIE_CodeBiere`) REFERENCES `lesaintbreuvage`.`prix`(`BIE_CodeBiere`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `prix` ADD CONSTRAINT `FK_biere_quantite` FOREIGN KEY (`QUA_CodeQuantite`) REFERENCES `lesaintbreuvage`.`prix`(`QUA_CodeQuantite`) ON DELETE CASCADE ON UPDATE CASCADE;
--- db/structure.sql
+++ db/structure.sql
@@ -2,3 +2,12 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `CAT_CodeCategorieBiere` varchar(3) NOT NULL,
   `CAT_LibelleCategorieBiere` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `lesaintbreuvage`.`biere` ( `BIE_CodeBiere` VARCHAR(3) NOT NULL , `BIE_DesignationBiere` VARCHAR(20) NOT NULL , `BIE_DescriptionBiere` VARCHAR(150) NOT NULL , `BIE_LienImageBiere` VARCHAR(100) NOT NULL , `CAT_CodeCategorieBiere` VARCHAR(3) NOT NULL ) ENGINE = InnoDB;
+
+CREATE TABLE `lesaintbreuvage`.`prix` ( `QUA_CodeQuantite` VARCHAR(3) NOT NULL , `QUA_LibelleQuantite` VARCHAR(50) NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE `lesaintbreuvage`.`quantite` ( `QUA_CodeQuantite` VARCHAR(3) NOT NULL , `BIE_CodeBiere` VARCHAR(3) NOT NULL , `PRX_Prix` VARCHAR(4) NOT NULL ) ENGINE = InnoDB;
+
+ALTER TABLE `biere` ADD PRIMARY KEY(`BIE_CodeBiere`);
+ALTER TABLE `categorie` ADD PRIMARY KEY(`CAT_CodeCategorieBiere`);
+ALTER TABLE `prix` ADD PRIMARY KEY(`BIE_CodeBiere`);
+ALTER TABLE `quantite` ADD PRIMARY KEY(`QUA_CodeQuantite`);
