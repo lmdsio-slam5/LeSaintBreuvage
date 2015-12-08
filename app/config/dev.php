5e0f413de7a887e2990dc7acb900207616b1dbe5
unknown <sylvie.pereira01@hotmail.fr>
2015-12-08 10:25:34 +0100

    Add Openshift

--- app/config/dev.php
+++ app/config/dev.php
@@ -1,13 +1,15 @@
-
-
 <?php
 
-//Ce fichier contient les options de configuration liés au développement de notre application. 
-// include the prod configuration
-
-require __DIR__.'/prod.php';
-
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => '127.0.0.1',
+    'port'     => '3306',
+    'dbname'   => 'presidents',
+    'user'     => 'presidents_user',
+    'password' => 'secret',
+);
 
 // enable the debug mode
-
 $app['debug'] = true;
